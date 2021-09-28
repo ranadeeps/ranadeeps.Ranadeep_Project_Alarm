eb9e8fed2d21cfb86b66a5a0d9f86e4002ea3141
ranadeeps <ranadeep2104@gmail.com>
2021-09-28 10:27:17 +0530

    select

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,29 @@
 setInterval(show,1000);
 function show(){
     const d = new Date();
-    document.getElementById("time").value = d.getHours() + ":" + d.getMinutes() + ":" +d.getSeconds();
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+    hour = hour < 10 ? "0" + hour : hour;
+    min= min< 10 ? "0" + min: min;
+    sec = sec < 10 ? "0" + sec : sec;
+    document.getElementById("time").value = hour + ":" + min+ ":" + sec;
 }
+function loadHours(){
+    let dropHours = document.getElementById('hours');
+    for (let i = 0; i <= 23; i++) {
+        let ele = document.createElement('option');
+        ele.text = i;
+        ele.value = i;
+        dropHours.options.add(ele);
+    }
+    let dropMin = document.getElementById('min');
+    for (let i = 0; i <= 59; i++) {
+        let ele = document.createElement('option');
+        ele.text = i;
+        ele.value = i;
+        dropMin.options.add(ele);
+    }
 
+}
+window.onload = loadHours();
